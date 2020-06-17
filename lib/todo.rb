6b8b2304d183af4e9f9c2cfc8a6f8d93b151c68a
kealanheena <kealanheena@gmail.com>
2020-06-17 11:26:33 +0100

    Chore: add test for adding multiple items to the list

--- lib/todo.rb
+++ lib/todo.rb
@@ -1,6 +1,16 @@
 class Todo
+
+  def initialize()
+    @list = []
+  end
+
   def alter(string)
+    items = ''
     task = string.split(" ")[1]
-    "1 #{task}"
+    @list << task
+    @list.each_with_index { |item, i|
+      items = items + "#{i + 1} #{item}\n"
+    }
+    items
   end
 end
--- spec/todo_spec.rb
+++ spec/todo_spec.rb
@@ -4,12 +4,24 @@ require 'todo.rb'
 
 describe Todo do
   describe '#alter' do
-    it 'adds an item' do
-      expect(subject.alter('add test')).to eq '1 test'
-    end
+    context 'adding one item' do
+      it 'adds an item' do
+        expect(subject.alter('add test')).to eq "1 test\n"
+      end
 
-    it 'adds dishes to list' do
-      expect(subject.alter('add dishes')).to eq '1 dishes'
+      it 'adds dishes to list' do
+        expect(subject.alter('add dishes')).to eq "1 dishes\n"
+      end
+
+      it 'adds dishes to list' do
+        expect(subject.alter('add laundry')).to eq "1 laundry\n"
+      end
+    end
+    context 'adding multiple items' do
+      it 'should list the items in numeric order' do
+        subject.alter('add test')
+        expect(subject.alter('add dishes')).to eq "1 test\n2 dishes\n"
+      end
     end
   end
 end
