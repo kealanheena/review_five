a930da85b0953facd31cb24ca561491ea4b1070b
kealanheena <kealanheena@gmail.com>
2020-06-18 14:23:00 +0100

    Refactor: remove functionality that gets the todos info

--- lib/todo.rb
+++ lib/todo.rb
@@ -4,15 +4,13 @@ class Todo
     @list = []
   end
 
-  def alter(string)
+  def alter(command)
     items = ''
-    item_array = string.split(" ")
-    action = item_array.shift()
-    task = item_array.join(' ')
-    if action == 'add'
-      add(task)
-    elsif action == 'done'
-      remove(task)
+    info = get_info(command)
+    if info[:action] == 'add'
+      add(info[:task])
+    elsif info[:action] == 'done'
+      remove(info[:task])
     end
     @list.each_with_index { |item, i|
       items = items + "#{i + 1} #{item}\n"
@@ -29,5 +27,13 @@ class Todo
   def remove(index)
     arr_index = index.to_i - 1
     @list.delete_at(arr_index)
+  end
+
+  def get_info(command)
+    command_array = command.split(" ")
+    info = {
+      action: command_array.shift(),
+      task: command_array.join(' ')
+    }
   end
 end
