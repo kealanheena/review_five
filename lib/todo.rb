8906e7d5e0b640dcf4256820b5b1a76f3c0deaca
kealanheena <kealanheena@gmail.com>
2020-06-18 14:16:46 +0100

    Refactor: seperate remove function

--- lib/todo.rb
+++ lib/todo.rb
@@ -10,13 +10,24 @@ class Todo
     action = item_array.shift()
     task = item_array.join(' ')
     if action == 'add'
-      @list << task
+      add(task)
     elsif action == 'done'
-      @list.delete_at(task.to_i - 1)
+      remove(task)
     end
     @list.each_with_index { |item, i|
       items = items + "#{i + 1} #{item}\n"
     }
     items
+  end
+
+  private
+
+  def add(item)
+    @list << item
+  end
+
+  def remove(index)
+    arr_index = index.to_i - 1
+    @list.delete_at(arr_index)
   end
 end
